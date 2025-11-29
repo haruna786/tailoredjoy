7b828d2f43cbf956300cb280c08dfa25c705e993
haruna786 <phru7197.86@gmail.com>
2025-11-29 23:26:49 +0900

    ポートフォリオ1129/1

--- Tailored Joy/js/script.js
+++ Tailored Joy/js/script.js
@@ -1,56 +1,85 @@
- // ハンバーガーメニュー
-        $(function () {
-            $(".hamburger").click(function () {
-                $(".hamburger").toggleClass("open");
-                // $(".header_nav").fadeToggle();
-            })
-        })
+// ハンバーガーメニュー
+$(function () {
+    $(".hamburger").click(function () {
+    $(".hamburger").toggleClass("open");
+    // $(".header_nav").fadeToggle();
+    });
+});
 
-        $(function () {
-            $(".hamburger-sp").click(function () {
-                $(".hamburger-sp").toggleClass("open-sp");
-                $(".header_nav-sp").fadeToggle();
-            })
-        });
+$(function () {
+    $(".hamburger-sp").click(function () {
+    $(".hamburger-sp").toggleClass("open-sp");
+    $(".header_nav-sp").fadeToggle();
+    });
+});
 
-         // slick
+// slick
 
-        // worksのスライド
-        $('.works_image').slick({
-            pauseOnHover: false,    // ホバーで止めない
-            centerMode: true,       // **中央の画像を強調 両サイドを表示**
-            centerPadding: "20%",   // **中央の画像の両端の表示領域を設定 (重要)**
-            lazyLoad: 'ondemand',
-            autoplay: true, //自動再生
-            autoplaySpeed: 4000,// 0秒間隔で動く
-            slidesToShow: 3,        // 一度に表示するスライドの数
-            slidesToScroll: 1,
-            dots: true,             // ドットナビゲーション
-            arrows: true,            // 矢印ナビゲーション
-            responsive: [{
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    centerPadding: '10%',
-                }
-            }]
-        });
+// worksのスライド
+$(".works_image").slick({
+  pauseOnHover: false, // ホバーで止めない
+  centerMode: true, // **中央の画像を強調 両サイドを表示**
+  centerPadding: "20%", // **中央の画像の両端の表示領域を設定 (重要)**
+    lazyLoad: "ondemand",
+  autoplay: true, //自動再生
+  autoplaySpeed: 4000, // 0秒間隔で動く
+  dots: true, // ドットナビゲーション
+  arrows: true, // 矢印ナビゲーション
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+        centerPadding: "10%"
+        }
+    }]
+});
+
+// イラストスライド
+$(".illust_image").slick({
+  arrows: true, // 横スクロールが消える
+    dots: false,
+  autoplay: true, //自動再生
+  autoplaySpeed: 0, // 0秒間隔で動く
+  slidesToShow: 10, //1度に10つのスライド表示
+  Infinity: true, //ループ
+  cssEase: "linear", //動きを滑らかに
+  speed: 5000, // スライド速度を調整
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+        slidesToShow: 7
+        }
+    }]
+});
+
+// ====================
+// popup表示
+// ====================
+
+$(".icon-circle").on("click", function () {
+    var targetPopup = $(this).closest(".icon-item").find(".popup");
+    targetPopup.fadeIn().addClass("show");
+});
+
+$(".close").on("click", function (event) {
+    event.stopPropagation();
+    $(this).closest(".popup").fadeOut().removeClass("show");
+});
+
+$(".popup").on("click", function (event) {
+    if ($(event.target).hasClass("popup")) {
+    $(this).fadeOut().removeClass("show");
+    }
+});
 
 
-        // イラストスライド
-        $(".illust_image").slick({
-            arrows: true,// 横スクロールが消える
-            dots: false,
-            autoplay: true, //自動再生
-            autoplaySpeed: 0,// 0秒間隔で動く
-            slidesToShow: 10, //1度に10つのスライド表示
-            Infinity: true,//ループ
-            cssEase: 'linear', //動きを滑らかに
-            speed: 5000, // スライド速度を調整
-            responsive: [{
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 7
-                }
-            }]
-        });
+// ワークス部分
+// 作品一覧の動作
+$(".works-right-push").on("click", function () {
+$(".works-right-push-menu").toggleClass("is-open");
+});
+
+// メニュー内のリンクをクリックした時も閉じる処理
+$(".works-menu-list a").on("click", function () {
+$(".works-right-push-menu").removeClass("is-open");
+$(".works-right-push").removeClass("open");
+});
