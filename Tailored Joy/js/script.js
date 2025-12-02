0a15bc6f62786f820c2cd19e178a49235ec81891
haruna786 <phru7197.86@gmail.com>
2025-12-03 00:20:27 +0900

    ポートフォリオ1203

--- Tailored Joy/js/script.js
+++ Tailored Joy/js/script.js
@@ -1,3 +1,7 @@
+// ====================
+// ⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎topページ⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎
+// ====================
+
 // ハンバーガーメニュー
 $(function () {
     $(".hamburger").click(function () {
@@ -13,8 +17,106 @@ $(function () {
     });
 });
 
-// slick
 
+// クリックすると犬の画像+背景が変わる
+// 定義
+const barking = document.getElementById("dog-click-image");
+const changing = document.getElementById("back-change");
+const changing2 = document.getElementById("works-back");
+const coloring = document.getElementById("back-color");
+// 動作
+function changeColor(){
+coloring.classList.toggle("on_back-color");
+}
+function changeImg(){
+barking.classList.toggle("on_dog-click");
+changing.classList.toggle("on_back-change");
+changing2.classList.toggle("on_works-back");
+}
+barking.addEventListener("click",changeImg);
+
+
+// ２つ目のナビゲーション
+$(function () {
+// コンセプトセクションの上端のY座標を取得
+// offset().top→ページの一番上からコンセプトセクションまでの距離
+    const conceptPos = $('#works').offset().top;
+    $(window).on('scroll', function() {
+        const scrollTop = $(window).scrollTop();
+        // スクロールがworksを過ぎたとき
+        if (scrollTop > conceptPos) {
+        // header_nav2 を表示
+            $('.header_nav2').addClass('active');
+        // ハンバーガーメニューのボタン自体を非表示
+            $('.hamburger').hide(); // .hide()で要素を非表示
+
+            // PCハンバーガーメニューが開いていたら自動で閉じる
+            if ($('.hamburger').hasClass('open')) {
+                // 開いていたメニュー（.header_nav）を閉じる
+                $('.header_nav').fadeOut(300);
+                // ボタンの開閉状態クラスを削除
+                $('.hamburger').removeClass('open');
+            }
+
+        } else {
+            // TOPに戻ったら header_nav2 を非表示
+            $('.header_nav2').removeClass('active');
+            // ★ ハンバーガーメニューのボタンを再表示する
+            $('.hamburger').show(); // .show()で要素を再表示
+        }
+    });
+
+});
+
+// 横スライド部分
+// HTMLドキュメント全体 の中から、セレクター(.sticky_wrap)に一致するすべての要素を
+// stickySections という名前の定数（再代入できない変数）として宣言
+// （height: 400vhを持つ要素）
+const stickySections = [...document.querySelectorAll('.sticky_wrap')]
+
+
+window.addEventListener('scroll', (e) => {
+    for(let i = 0; i < stickySections.length; i++){
+    transform(stickySections[i])
+    }
+})
+
+function transform(section) {
+
+    const offsetTop = section.parentElement.offsetTop;
+    const scrollSection = section.querySelector('.horizontal_scroll')
+
+  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+
+    percentage = percentage < 0 ? 0 : percentage > 100 ? 100 : percentage;
+    scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`
+}
+
+// // 犬がスクロールに合わせて大きくなる
+
+$(function () {
+    /* スクロール量　代入用 */
+    var scroll;
+    var sum;
+    $(window).scroll(function () {
+        /* スクロール量を取得 */
+        scroll = $(this).scrollTop();
+        /* コンソールログで確認  */
+        console.log(scroll);
+        /* 円の大きさ+スクロール量 / 60*/
+        sum = 1 + scroll / 80;
+        console.log(sum);
+        /* 円の大きさにスクロール量を足す */
+        $(".image-item-dog").css({
+            "transform": "scale(" + sum + ")"
+        })
+    })
+})
+
+
+
+
+// slick
 // worksのスライド
 $(".works_image").slick({
   pauseOnHover: false, // ホバーで止めない
@@ -51,8 +153,10 @@ $(".illust_image").slick({
     }]
 });
 
+
+
 // ====================
-// popup表示
+// アバウトページ　popup表示
 // ====================
 
 $(".icon-circle").on("click", function () {
@@ -72,8 +176,10 @@ $(".popup").on("click", function (event) {
 });
 
 
-// ワークス部分
-// 作品一覧の動作
+// ====================
+// ワークスページ　SPの横に出てくる表示
+// ====================
+// 「作品一覧」の動作
 $(".works-right-push").on("click", function () {
 $(".works-right-push-menu").toggleClass("is-open");
 });
@@ -85,69 +191,3 @@ $(".works-right-push").removeClass("open");
 });
 
 
-
-
-// ====================
-// TOP
-// 横スライド部分
-// ====================
-
-
-
-// HTMLドキュメント全体 の中から、セレクター(.sticky_wrap)に一致するすべての要素を
-// stickySections という名前の定数（再代入できない変数）として宣言
-// （height: 400vhを持つ要素）
-const stickySections = [...document.querySelectorAll('.sticky_wrap')]
-
-
-window.addEventListener('scroll', (e) => {
-  for(let i = 0; i < stickySections.length; i++){
-    transform(stickySections[i])
-  }
-})
-
-function transform(section) {
-
-  const offsetTop = section.parentElement.offsetTop;
-  const scrollSection = section.querySelector('.horizontal_scroll')
-
-  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
-
-  percentage = percentage < 0 ? 0 : percentage > 100 ? 100 : percentage;
-  scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`
-}
-
-// // 犬がスクロールに合わせて大きくなる
-
-$(function () {
-    /* スクロール量　代入用 */
-    var scroll;
-    /* 円の大きさ+スクロール量　代入用 */
-    var sum;
-    $(window).scroll(function () {
-        /* スクロール量を取得 */
-        scroll = $(this).scrollTop();
-        /* コンソールログで確認  */
-        console.log(scroll);
-        /* 円の大きさ+スクロール量 / 60*/
-        sum = 1 + scroll / 60;
-        console.log(sum);
-        /* 円の大きさにスクロール量を足す */
-        $(".image-item-dog").css({
-            "transform": "scale(" + sum + ")"
-        })
-    })
-})
-
-
-// クリックすると犬の画像+背景が変わる
-const barking = document.getElementById("dog-click-image");
-const changing = document.getElementById("back-change");
-const changing2 = document.getElementById("works-back");
-
-function changeImg(){
-barking.classList.toggle("on_dog-click");
-changing.classList.toggle("on_back-change");
-changing2.classList.toggle("on_works-back");
-}
-barking.addEventListener("click",changeImg);
